e8f5dc7ede4cd853f54bacae01991490c25417c2
casanchez896 <54144271+casanchez896@users.noreply.github.com>
2019-08-15 11:54:10 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _100PersonalizadoTres.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
